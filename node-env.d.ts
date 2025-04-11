8947f6383529a53656f5f19a0d761a5ef445f49c
Andreas Kvist <kvistcake@gmail.com>
2025-04-11 06:50:50 +0200

    jira token declaration

--- node-env.d.ts
+++ node-env.d.ts
@@ -2,6 +2,7 @@ declare namespace NodeJS {
     interface ProcessEnv {
         XDG_RUNTIME_DIR: string;
         TOGGL_API_TOKEN: string;
+        JIRA_TOKEN: string;
         HOME: string;
         TERMINAL: string;
         UID: string;
